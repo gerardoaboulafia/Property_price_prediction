ea91e69d5d2758d203f17e1cea5daa43526acefa
Gerardo Aboulafia <gerardoaboulafia@uca.edu.ar>
2025-09-01 23:39:50 -0300

    las funciones fueron modificadas levemente y se avanzó con el testing de la pipeline

--- Pipeline/preprocess/filter_data.py
+++ Pipeline/preprocess/filter_data.py
@@ -2,7 +2,7 @@ def filter_by_currency_place(data):
     """
     La función filter_by_currency_place filtra el DataFrame según la moneda y la ubicación.
     Toma como parámetro un DataFrame de pandas.
-    Asume que las columnas 'currency' y 'l2' existen en el DataFrame.
+    Asume que las columnas 'property_type' y 'l2' existen en el DataFrame.
     Devuelve el DataFrame con los casos flageados.
     """
     # Initialize flag column
@@ -11,8 +11,8 @@ def filter_by_currency_place(data):
     # Create separate flag conditions
     currency_flag = data['currency'] != 'USD'
     location_flag = data['l2'] != 'Capital Federal'
-    property_type_flag = ~data['type'].isin(['Departamento', 'Casa', 'PH'])
-    
+    property_type_flag = ~data['property_type'].isin(['Departamento', 'Casa', 'PH'])
+
     # Build flag messages by combining conditions
     data.loc[currency_flag, 'flag'] += 'Non-USD currency; '
     data.loc[location_flag, 'flag'] += 'Not in Capital Federal; '
